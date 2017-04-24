82544f3a1929ad2d76b891d3105fd07a533a9e32
JinliWangKolmostar <jingli.wang@kolmostar.com>
2017-04-24 11:48:36 +0800

    code refactor

--- data_conver.h
+++
@@ -1,7 +0,0 @@
-#ifndef DATA_CONVER_H_INCLUDED
-#define DATA_CONVER_H_INCLUDED
-
-int DataFormatConverse_A();
-int DataFormatConverse_B();
-
-#endif // DATA_CONVER_H_INCLUDED
--- find_substring.h
+++
@@ -1,7 +0,0 @@
-#ifndef FIND_SUBSTRING_H_INCLUDED
-#define FIND_SUBSTRING_H_INCLUDED
-
-int FindSubstring(char *source_file_name);
-void match_data();
-
-#endif // FIND_SUBSTRING_H_INCLUDED
